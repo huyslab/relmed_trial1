98011645039468f0e115743be2267329634a0bf7
yanivabir <yanivabir@gmail.com>
2025-12-16 14:48:18 -0500

    Revert "fix: update allowed origins to include beta environment"

--- utils.js
+++ utils.js
@@ -210,8 +210,7 @@ function postToParent(message, fallback = () => {}) {
             const allowedOrigins = [
                 'http://localhost:3000',
                 'https://relmed.ac.uk',
-                'https://www.relmed.ac.uk',
-                'https://beta.relmed.ac.uk'
+                'https://www.relmed.ac.uk'
             ];
 
             // Normalize a URL by removing trailing slashes
